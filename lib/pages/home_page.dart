d651f873e0bb55badfbde19d38f226b0a034e2e0
zahid2873 <mahdi.zhm13@gmail.com>
2023-08-02 22:17:32 +0600

    api integrate and display in Posts home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,97 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+import '../models/posts_model.dart';
+import 'package:http/http.dart' as http;
+
+class PostsHomePage extends StatefulWidget {
+  const PostsHomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<PostsHomePage> createState() => _PostsHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _PostsHomePageState extends State<PostsHomePage> {
+  List<PostsModel> postsList = [];
+
+  Future <List<PostsModel>> getPostsApi() async{
+    String url = "https://jsonplaceholder.typicode.com/posts";
+    final response = await http.get(Uri.parse(url));
+    final data = jsonDecode(response.body.toString());
+    if(response.statusCode == 200){
+      for (dynamic i in data){
+        postsList.add(PostsModel.fromJson(i));
+      }
+      return postsList;
+    }else{
+      return postsList;
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Posts"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostsApi(),
+              builder: (context, snapshot){
+                if(!snapshot.hasData){
+                  return Center(child: CircularProgressIndicator());
+                } else{
+                  return ListView.builder(
+                      itemCount: postsList.length,
+                      itemBuilder: (context, index){
+                    return Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height *.3,
+                        child: Card(
+                          elevation: 20,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          color: Colors.green.withOpacity(.7),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(postsList[index].title!, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text("User Id: ${postsList[index].userId!}", style: TextStyle(color: Colors.black.withOpacity(.5),fontSize: 12, fontWeight: FontWeight.bold),),
+                                      Spacer(),
+                                      Text(" Id: ${postsList[index].id!}", style: TextStyle(color: Colors.black.withOpacity(.5),fontSize: 12, fontWeight: FontWeight.bold),),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5,),
+                                  Text(postsList[index].body!, style: TextStyle(fontSize: 12,),),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+                }
+
+              }
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
